46504891941ecca159819e38ae38e077472c21da
zairdon20 <josphat2020@gmail.com>
2022-05-06 21:29:22 +0200

    Setup project

---
+++ config/routes.rb
@@ -0,0 +1,15 @@
+Rails.application.routes.draw do
+  root 'users#index'
+
+  resources :users, only: [:index, :show] do 
+    resources :posts, only: [:index, :show]
+  end
+  # get 'posts/index'
+  # get 'posts/show'
+  # get 'users/index'
+  # get 'users/show'
+  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  # Defines the root path route ("/")
+  # root "articles#index"
+end

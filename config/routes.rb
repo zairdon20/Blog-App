fc7fb70657eb7ba5d0cf1ce421800a57c4968a90
zairdon20 <josphat2020@gmail.com>
2022-05-17 00:36:35 +0200

    remove commented code

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,22 @@
 Rails.application.routes.draw do
-  # devise_for :models
-  # devise_for :views
-  # root 'pages#home'
+  devise_for :models
+  devise_for :views
+  root 'pages#home'
   devise_for :users
-  # root "home#index"
+  root "home#index"
   root 'pages#home'
   resources :likes
-  # resources :users, only: [:index, :show] do
-  #   resources :posts, only: [:index, :new, :show, :create]
+  resources :users, only: [:index, :show] do
+    resources :posts, only: [:index, :new, :show, :create]
   end
 
-  root 
+  root "users#index"
+  root to: "home#index" 
 
-#   resources :posts do
-#     resources :comments, only: [:create]
-#     resources :likes, only: [:create]
-#   end
-# end
+
+
+  resources :posts do
+    resources :comments, only: [:create]
+    resources :likes, only: [:create]
+  end
+end

6fc2aaa13a5250db03ef5a11f3a5663fe2360d58
zairdon20 <josphat2020@gmail.com>
2022-05-17 11:48:46 +0200

    Add user confirmation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,15 @@
 class ApplicationController < ActionController::Base
-  def 
-    User.first
+  add_flash_types :danger, :info, :warning, :success, :messages, :notice, :alert
+  protect_from_forgery with: :exception
+
+  before_action :authenticate_user!
+  before_action :authorized, only: [:auto_login]
+  before_action :update_allowed_parameters, if: :devise_controller?
+
+  protected
+
+  def update_allowed_parameters
+    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :posts_counter, :email, :password) }
+    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :email, :password, :current_password) }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  devise_for :models
-  devise_for :views
-  root 'pages#home'
+  # root 'pages#home'
   devise_for :users
-  root "home#index"
-  root 'pages#home'
+  # root "home#index"
+  # root 'pages#home'
   resources :likes
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :new, :show, :create]
   end
 
   root "users#index"
-  root to: "home#index" 
+  # root to: "home#index" 
 
 
 
